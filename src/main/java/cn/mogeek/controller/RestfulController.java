2aabb4d0216a10a5db19caebc92c84f4b8a8b761
OwlWinter <572232978@qq.com>
2020-05-22 19:25:10 +0800

    对接口做了校验并用 messageSource 包装了返回的信息
    1 接口返回数据有改动，README 暂时没改
    2 meaasgeSource 测试没有问题
    3 配置文件只用到了 message_zh_CN.properties
    4 后期需要对 controller 的返回数据做一个包装再合并到主分支

--- src/main/java/cn/mogeek/controller/RestfulController.java
+++ src/main/java/cn/mogeek/controller/RestfulController.java
@@ -2,6 +2,7 @@ package cn.mogeek.controller;
 
 import cn.mogeek.model.Disciple;
 import cn.mogeek.service.DiscipleService;
+//import com.sun.org.apache.xpath.internal.operations.String;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.dao.DuplicateKeyException;
@@ -37,64 +38,116 @@ public class RestfulController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public @ResponseBody Map<String, Object> getPage(int page, int per_page){
         int page_size = 10;
-        Map<String, Object> stringObjectMap = new HashMap<>();
+        Map<String, Object> response = new HashMap<>();
         List<Disciple> buffer_disciple_list = null;
         try {
             buffer_disciple_list = service.query(new Disciple());
-
             per_page = (buffer_disciple_list.size() + page_size - 1)/page_size;
             List<Disciple> discipleList = null;
-            if (page*page_size > buffer_disciple_list.size()){
-                discipleList = buffer_disciple_list.subList((page-1)*page_size, buffer_disciple_list.size());
-            }else {
-                discipleList = buffer_disciple_list.subList((page-1)*page_size, page*page_size);
-            }
 
-            stringObjectMap.put("code", 200);
-            stringObjectMap.put("data", discipleList);
-            stringObjectMap.put("count", buffer_disciple_list.size());
-            stringObjectMap.put("page", page);
-            stringObjectMap.put("per_page", per_page);
+            if (buffer_disciple_list.size() == 0){
+                response.put("code", 204);
+                response.put("msg", messageSource.getMessage("Empty_list",
+                        null, Locale.getDefault()));
+            }else if ((page - 1)*page_size > buffer_disciple_list.size() || page <= 0){
+                response.put("code", 400);
+                response.put("count", buffer_disciple_list.size());
+                response.put("page", page);
+                response.put("per_page", per_page);
+                response.put("msg", messageSource.getMessage("page.Out_of_index",
+                        null, Locale.getDefault()));
+            } else {
+                if (page*page_size > buffer_disciple_list.size()){
+                    discipleList = buffer_disciple_list.subList((page-1)*page_size, buffer_disciple_list.size());
+                }else {
+                    discipleList = buffer_disciple_list.subList((page-1)*page_size, page*page_size);
+                }
+
+                response.put("code", 200);
+                response.put("data", discipleList);
+                response.put("count", buffer_disciple_list.size());
+                response.put("page", page);
+                response.put("per_page", per_page);
+                response.put("msg", messageSource.getMessage("page.Success",
+                        null, Locale.getDefault()));
+            }
         } catch (Exception e) {
-            stringObjectMap.put("code", 500);
+            response.put("code", 500);
+            response.put("msg", messageSource.getMessage("page.Fail",
+                    null, Locale.getDefault()));
             e.printStackTrace();
         }
-
-
-        return stringObjectMap;
+        return response;
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public @ResponseBody Map<String, Object> addDisciple(@RequestBody Disciple disciple){
+        System.out.println("\n=========\n" + disciple.getStudent_id() + ":" + disciple.getStudent_name());
         Map<String, Object> response = new HashMap<>();
-        try {
-            service.insert(disciple);
-            Integer id = disciple.getId();
-            response.put("code", 201);
-            response.put("data", disciple);
-            response.put("msg", messageSource.getMessage("post.success", null, Locale.getDefault()));
-        }catch (DuplicateKeyException exception){
+
+        if (disciple.getStudent_id() == null){
             response.put("code", 400);
-            response.put("msg", messageSource.getMessage("post.student_id_repeatedly", null, Locale.getDefault()));
-        }catch (Exception e) {
-            e.printStackTrace();
+            response.put("msg", messageSource.getMessage("Missing_field",
+                    new String[]{"student_id"}, Locale.getDefault()));
+        } else if (disciple.getStudent_name() == null){
             response.put("code", 400);
-            response.put("msg", messageSource.getMessage("post.fail", null, Locale.getDefault()));
+            response.put("msg", messageSource.getMessage("Missing_field",
+                    new String[]{"student_name"}, Locale.getDefault()));
+        } else {
+            try {
+                service.insert(disciple);
+                Integer id = disciple.getId();
+                response.put("code", 201);
+                response.put("data", disciple);
+                response.put("msg", messageSource.getMessage("insert.Success",
+                        null, Locale.getDefault()));
+            }catch (DuplicateKeyException e){
+                response.put("code", 400);
+                response.put("msg", messageSource.getMessage("Student_id_repeatedly",
+                        new String[]{"student_id", disciple.getStudent_id().toString()}, Locale.getDefault()));
+            }catch (Exception e) {
+                e.printStackTrace();
+                response.put("code", 500);
+                response.put("msg", messageSource.getMessage("insert.Fail",
+                        null, Locale.getDefault()));
+            }
         }
+
         return response;
     }
 
     @RequestMapping(value = "", method = RequestMethod.PUT)
     public @ResponseBody Map<String, Object> updateDisciple(@RequestBody Disciple disciple){
         Map<String, Object> response = new HashMap<>();
-        try {
-            boolean status = service.update(disciple);
-            int code = status ? 201 : 404;
-            response.put("code", code);
-        } catch (Exception e) {
-            e.printStackTrace();
-            response.put("code", 500);
+
+        if (disciple.getId() == null){
+            response.put("code", 400);
+            response.put("msg", messageSource.getMessage("Missing_field",
+                    new String[]{"id"}, Locale.getDefault()));
+        }else {
+            try {
+                boolean status = service.update(disciple);
+                if (status){
+                    response.put("code", 201);
+                    response.put("msg", messageSource.getMessage("update.Success",
+                            null, Locale.getDefault()));
+                }else {
+                    response.put("code", 204);
+                    response.put("msg", messageSource.getMessage("ID_does_not_exist",
+                            new String[]{"id", disciple.getId().toString()}, Locale.getDefault()));
+                }
+            }catch (DuplicateKeyException e){
+                response.put("code", 400);
+                response.put("msg", messageSource.getMessage("Student_id_repeatedly",
+                        new String[]{"student_id", disciple.getStudent_id().toString()}, Locale.getDefault()));
+            }catch (Exception e) {
+                e.printStackTrace();
+                response.put("code", 500);
+                response.put("msg", messageSource.getMessage("update.Fail",
+                        null, Locale.getDefault()));
+            }
         }
+
         return response;
     }
 
@@ -106,36 +159,56 @@ public class RestfulController {
         System.out.println("---------------");
         Disciple disciple = new Disciple();
         disciple.setId(id);
+        Map<String, Object> response = new HashMap<>();
 
-        Map<String, Object> stringObjectMap = new HashMap<>();
         try {
             List<Disciple> discipleList = service.query(disciple);
             int count = discipleList.size();
             disciple = discipleList.get(0);
-            int code = count == 1 ? 200 : 404;
-            stringObjectMap.put("code", code);
             if (count == 1){
-                stringObjectMap.put("disciple", disciple);
+                response.put("code", 200);
+                response.put("disciple", disciple);
+                response.put("msg", messageSource.getMessage("query_by_id.Success",
+                        null, Locale.getDefault()));
+            }else {
+                response.put("code", 204);
+                response.put("msg", messageSource.getMessage("ID_does_not_exist",
+                        new String[]{"id", id.toString()}, Locale.getDefault()));
             }
-        } catch (Exception e) {
+        }catch (IndexOutOfBoundsException e){
+            response.put("code", 204);
+            response.put("msg", messageSource.getMessage("ID_does_not_exist",
+                    new String[]{"id", id.toString()}, Locale.getDefault()));
+        }catch (Exception e) {
             e.printStackTrace();
-            stringObjectMap.put("code", 500);
+            response.put("code", 500);
+            response.put("msg", messageSource.getMessage("query_by_id.Fail",
+                    null, Locale.getDefault()));
         }
-        return stringObjectMap;
+        return response;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public @ResponseBody Map<String, Object> deletedisciple(@PathVariable("id") Integer id){
-        Map<String, Object> stringObjectMap = new HashMap<>();
+        Map<String, Object> response = new HashMap<>();
         try {
             boolean status = service.delete(id);
-            int code = status ? 204 : 404;
-            stringObjectMap.put("code", code);
+            if (status){
+                response.put("code", 204);
+                response.put("msg", messageSource.getMessage("delete.Success",
+                        null, Locale.getDefault()));
+            }else {
+                response.put("code", 410);
+                response.put("msg", messageSource.getMessage("ID_does_not_exist",
+                        new String[]{"id", id.toString()}, Locale.getDefault()));
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            stringObjectMap.put("code", 500);
+            response.put("code", 500);
+            response.put("msg", messageSource.getMessage("delete.Fail",
+                    null, Locale.getDefault()));
         }
-        return stringObjectMap;
+        return response;
     }
 
 }
